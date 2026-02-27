f3e34b9d750d7ebb0b73b07c6e87d59321a6747c
morfesla <morfexsla@gmail.com>
2026-02-27 20:42:50 -0300

    realizada questão dois

---
+++ Ativida.py
@@ -0,0 +1,14 @@
+import os
+os.system("cls")
+
+num = int(input("Escreva um numero: "))
+
+
+if num <10:
+    print("é menor que 10")
+elif num == 10:
+    print("é igual a 10")    
+else:
+    print("é maior que 10")
+
+        
